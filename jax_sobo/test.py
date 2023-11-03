7957bb271a4c1cb2ca69ddce53e7385871b24d2e
zihao wang <ge65soh@mytum.de>
2023-11-03 20:42:19 +0100

    change path

--- jax_sobo/test.py
+++
@@ -1,8 +0,0 @@
-import gp
-import jax.numpy as jnp
-from jax import jacrev, jit, lax, random, tree_map, vmap
-import matplotlib.pyplot as plt
-from jax import random
-from functools import partial
-import numpy as np
-
