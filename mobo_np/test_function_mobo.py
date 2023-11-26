18be5c5d4a3503f5892b368b7f4bed65cf489d30
Zihao Wang <ge65soh@mytum.de>
2023-11-26 22:00:41 +0100

    update

--- mobo_np/test_function_mobo.py
+++ mobo_np/test_function_mobo.py
@@ -1,4 +1,4 @@
-import jax.numpy as jnp
+import numpy as np
 
 def schaffer_function_n1_multiobjective(x):
     """
@@ -12,7 +12,7 @@ def schaffer_function_n1_multiobjective(x):
     """
     f1 = x**2
     f2 = (x - 2)**2
-    return jnp.array([f1, f2])
+    return np.array([f1, f2])
 
 def kursawe_function(x):
     """
@@ -24,7 +24,7 @@ def kursawe_function(x):
     Returns:
     numpy array: The results of the Kursawe Function
     """
-    f1 = -10 * jnp.exp(-0.2 * jnp.sqrt(x[0]**2 + x[1]**2))
-    f2 = (jnp.abs(x[0])**0.8 + 5 * jnp.sin(x[0]**3) + 3 * jnp.cos(5 * x[0])) +\
-         (jnp.abs(x[1])**0.8 + 5 * jnp.sin(x[1]**3) + 3 * jnp.cos(5 * x[1]))
+    f1 = -10 * np.exp(-0.2 * np.sqrt(x[0]**2 + x[1]**2))
+    f2 = (np.abs(x[0])**0.8 + 5 * np.sin(x[0]**3) + 3 * np.cos(5 * x[0])) +\
+         (np.abs(x[1])**0.8 + 5 * np.sin(x[1]**3) + 3 * np.cos(5 * x[1]))
     return np.array([f1, f2])
