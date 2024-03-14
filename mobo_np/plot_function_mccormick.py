c374dddfed936d202181a11091338329bc888bfc
zihao wang <ge65soh@mytum.de>
2024-03-14 21:10:55 +0100

    update for mc figure

--- mobo_np/plot_function_mccormick.py
+++ mobo_np/plot_function_mccormick.py
@@ -15,23 +15,21 @@ z = mccormick_function(x, y)
 
 # 绘制图形
 fig, ax = plt.subplots()
-contour = ax.contourf(x, y, z, cmap='viridis')
-cbar = plt.colorbar(contour, label='Function Value')
+contour = ax.contourf(x, y, z, levels=1000, cmap='viridis')
+cbar = plt.colorbar(contour)
 
 # 设置指数刻度
-cbar.formatter.set_powerlimits((-2, 2))
-cbar.update_ticks()
+cbar.set_ticks(np.arange(0, 36, 5))  # 这里设置了从0到30，每5个单位一个刻度
+
 
 # 添加等高线
-contour_lines = plt.contour(x, y, z, colors='black', linewidths=0.5)
+contour_lines = plt.contour(x, y, z, colors='white', linewidths=0.5)
 plt.clabel(contour_lines, inline=True, fontsize=8)
 
 # 添加标签和标题
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.title('McCormick Function')
 
 # 显示图形
-plt.show()
-
-plt.savefig('/home/wzhmiasanmia/ma_workspace/jax_mobo/mobo_np/test.png')
+plt.savefig('/home/wzhmiasanmia/ma_workspace/jax_mobo/mobo_np/McCormick.png')
